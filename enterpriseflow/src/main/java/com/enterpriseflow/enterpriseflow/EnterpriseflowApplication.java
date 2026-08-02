bb7c4a39bfcb180c711db858e46acb420e4ac0e0
Naveen S <naveen121101@gmail.com>
2026-08-02 12:09:14 +0530

    First commit of Spring Boot with postgresql setup

---
+++ enterpriseflow/src/main/java/com/enterpriseflow/enterpriseflow/EnterpriseflowApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseflow.enterpriseflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseflowApplication.class, args);
+	}
+
+}
