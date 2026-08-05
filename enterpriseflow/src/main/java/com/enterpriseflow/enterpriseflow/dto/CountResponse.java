40d0d6bf424d06c5d209ef370b33af5f4978a3a7
Naveen S <naveen121101@gmail.com>
2026-08-05 23:34:50 +0530

    Add Count endpoint for heath controller

---
+++ enterpriseflow/src/main/java/com/enterpriseflow/enterpriseflow/dto/CountResponse.java
@@ -0,0 +1,15 @@
+package com.enterpriseflow.enterpriseflow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+public class CountResponse {
+    int count;
+
+}
